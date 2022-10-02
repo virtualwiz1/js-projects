b8a44d799b3e877f2227000ecbb7ee87b9659afa
Virtual Wiz <101511280+virtualwiz1@users.noreply.github.com>
2022-10-02 16:35:42 +0545

    Update script.js

--- dictionaryapp/script.js
+++ dictionaryapp/script.js
@@ -24,9 +24,9 @@ btn.addEventListener("click", () => {
                    ${data[0].meanings[0].definitions[0].definition}
                 </p>
                 <p class="word-example">
-                    ${data[0].meanings[0].definitions[0].example || ""}
+                    ${data[0].meanings[0].definitions[1].example || ""}
                 </p>`;
-            sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+            sound.setAttribute("src", `${data[0].phonetics[1].audio}`);
         })
         .catch(() => {
             result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
